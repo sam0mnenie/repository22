e4251e24d9926b21f3a19f092c1e9896e6f8dc02
skaylean4 <skaylean444@gmail.com>
2024-01-24 13:17:07 +0200

    add task3:newalgoritm

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 7;
+int b = 10;
+int c = 15;
+int d = 5;
+int e = 30;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
